5ebf4a00ba491d0256a6439dbfccdfb4faf9cf45
SerPan23 <igracom@yandex.ru>
2020-11-19 13:13:29 +0300

    u

--- Sortic/Sortic/Sortic.cpp
+++ Sortic/Sortic/Sortic.cpp
@@ -1,6 +1,17 @@
 #include "../../func/sortic.h"
 int main()
 {
-    std::cout << "Hello World!\n";
+    vector <int> mass;
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        int tmp;
+        cin >> tmp;
+        mass.push_back(tmp);
+    }
+    sa(mass);
+    for (int i = 0; i < n; i++) {
+        cout << mass[i];
+    }
 }
 
--- func/ss.cpp
+++ func/ss.cpp
@@ -1,11 +1,19 @@
 #include "sortic.h"
 void sa(vector <int>& a) {
-
+	if (a.size() >= 2) {
+		int tmp = a[1];
+		a[1] = a[0];
+		a[0] = tmp;
+	}
 }
 
 
 void sb(vector <int>& b) {
-
+	if (b.size() >= 2) {
+		int tmp = b[1];
+		b[1] = b[0];
+		b[0] = tmp;
+	}
 }
 
 
