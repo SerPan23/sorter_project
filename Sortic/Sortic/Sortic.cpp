47bf3973954d8155d3362442d97859d33d285846
SerPan23 <igracom@yandex.ru>
2020-11-19 14:41:10 +0300

    u

--- Sortic/Sortic/Sortic.cpp
+++ Sortic/Sortic/Sortic.cpp
@@ -1,17 +1,31 @@
 #include "../../func/sortic.h"
 int main()
 {
-    vector <int> mass;
+    vector <int> a;
+    vector <int> b;
     int n;
     cin >> n;
     for (int i = 0; i < n; i++) {
         int tmp;
         cin >> tmp;
-        mass.push_back(tmp);
+        a.push_back(tmp);
     }
-    sa(mass);
+    int m;
+    cin >> m;
+    for (int i = 0; i < m; i++) {
+        int tmp;
+        cin >> tmp;
+        a.push_back(tmp);
+    }
+
+    pa(a, b);
+
     for (int i = 0; i < n; i++) {
-        cout << mass[i];
+        cout << a[i] << " ";
+    }
+    cout << endl;
+    for (int i = 0; i < m; i++) {
+        cout << b[i] << " ";
     }
 }
 
--- func/papb.cpp
+++ func/papb.cpp
@@ -1,9 +1,28 @@
 #include "sortic.h"
 void pa(vector <int>& a, vector <int>& b) {
-
+	if (b.size() > 0) {
+		int tmp = b[0];
+		int tmp2 = 0;
+		for (int i = 0; i < a.size()+1; i++) {
+			tmp2 = a[i];
+			a[i] = tmp;
+			tmp = tmp2;
+		}
+		a.push_back(tmp);
+	}
 }
 
 
 void pb(vector <int>& a, vector <int>& b) {
-
+	vector <int> otv(a.size() + 1);
+	if (a.size() > 0) {
+		int tmp = a[0];
+		int tmp2 = 0;
+		for (int i = 0; i < b.size(); i++) {
+			tmp2 = b[i];
+			b[i] = tmp;
+			tmp = tmp2;
+		}
+		b.push_back(tmp);
+	}
 }
