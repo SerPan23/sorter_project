c38c3e065187e59bd952b6453ea00e596b2da1d2
SerPan23 <igracom@yandex.ru>
2020-11-28 20:14:22 +0300

    u

--- Check_sortic/Check_sortic/Check_sortic.cpp
+++ Check_sortic/Check_sortic/Check_sortic.cpp
@@ -5,7 +5,6 @@ int main()
     vector <int> a_sort;
     vector <int> b;
     fill_array(a);
-    //fill_array(b);
     simple_sort(a, a_sort);
 
     cout_array(a);
---
+++ func/command_sort.cpp
@@ -0,0 +1,8 @@
+#include "sortic.h"
+void command_sort(vector <int>& a, vector <int>& b) {
+	string str;
+	cin >> str;
+	while (str != "*") {
+
+	}
+}
