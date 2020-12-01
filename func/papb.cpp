fade9e4fbee5a27a303198d3b77f60670b838163
SerPan23 <igracom@yandex.ru>
2020-12-01 11:41:17 +0300

    u

--- func/papb.cpp
+++ func/papb.cpp
@@ -1,17 +1,5 @@
 #include "sortic.h"
 
-void reverse_vector(vector <int>& arr) {
-	int left = 0, right = arr.size() - 1;
-	for (int i = 0; i < arr.size() / 2; i++)
-	{
-		int temp = arr[left];
-		arr[left] = arr[right];
-		arr[right] = temp;
-		left++;
-		right--;
-	}
-}
-
 void pa(vector <int>& a, vector <int>& b) {
 	//cout << b.size() << endl;
 	if (b.size() > 0) {
--- func/reverse_vector.cpp
+++ func/reverse_vector.cpp
@@ -1,13 +1,13 @@
 #include "sortic.h"
 
-//void reverse_vector(vector <int>& arr) {
-//	int left = 0, right = arr.size() - 1;
-//	for (int i = 0; i < arr.size() / 2; i++)
-//	{
-//		int temp = arr[left];
-//		arr[left] = arr[right];
-//		arr[right] = temp;
-//		left++;
-//		right--;
-//	}
-//}
+void reverse_vector(vector <int>& arr) {
+	int left = 0, right = arr.size() - 1;
+	for (int i = 0; i < arr.size() / 2; i++)
+	{
+		int temp = arr[left];
+		arr[left] = arr[right];
+		arr[right] = temp;
+		left++;
+		right--;
+	}
+}
